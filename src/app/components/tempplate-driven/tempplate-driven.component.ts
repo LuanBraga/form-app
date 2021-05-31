23febe16bf2fb2f3463a3911e0bf2e1b0172d7c5
Luan Braga <luanbraga.lb@gmail.com>
2021-05-31 16:30:26 -0300

    add onSubmit() function

--- src/app/components/tempplate-driven/tempplate-driven.component.ts
+++ src/app/components/tempplate-driven/tempplate-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tempplate-driven',
@@ -10,6 +11,10 @@ export class TempplateDrivenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(myForm: NgForm){
+    console.log(myForm)
   }
 
 }
